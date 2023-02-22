c2ff9cf9205f7221999cb4e44f254ac28c9bb796
Tomeu <Tomeu@PC-MASTER-RACE-TOMEU>
2023-02-22 22:18:16 +0100

    Segundo commit del repositorio Deposito

--- src/cuentas/Main.java
+++ src/cuentas/Main.java
@@ -20,10 +20,10 @@ public class Main {
 
     private static void operativa_cuenta(float cantidad) {
         CCuenta cuenta1;
-        double saldoActual;
+        double saldoActual1;
         cuenta1 = new CCuenta("Antonio López","1000-2365-85-1230456789",2500,0);
-        saldoActual = cuenta1.estado();
-        System.out.println("El saldo actual es"+ saldoActual );
+        saldoActual1 = cuenta1.estado();
+        System.out.println("El saldo actual es"+ saldoActual1 );
         try {
             cuenta1.retirar(2300);
         } catch (Exception e) {
